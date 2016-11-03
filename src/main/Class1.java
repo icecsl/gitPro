c56c9f56503295eedfd01bb0caf5f5d5e6e20bf6
ice <1058232569@qq.com>
2016-11-03 15:31:23 +0800

    create gitPro

---
+++ src/main/Class1.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by huangbingjing on 16/11/3.
+ */
+public class Class1 {
+    public static void main(String[] args) {
+        System.out.println("master");
+    }
+}
